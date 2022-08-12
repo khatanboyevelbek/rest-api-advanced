dfd5d808797ddd038be9bb6f2e10d34118b9ed00
Elbek Khatanboyev <programmerelbek@gmail.com>
2022-08-12 16:25:18 +0500

    New features

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const path = require('path');
 const multer = require('multer');
 const helmet = require('helmet');
+const morgan = require('morgan');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const server = require('http').Server(app);
 const port = process.env.PORT;
@@ -17,14 +20,22 @@ const corsConfig = {
     credential: true
 }
 
+const morganLogStream = fs.createWriteStream(path.join(__dirname, '/logStream.log'),{flags: 'a'});
+// const privateKey = fs.readFileSync('./server.key');
+// const publicKey = fs.readFileSync('./server.cert');
+
 app.use(express.json());
 app.use(multer({storage: multerConfig.storage, fileFilter: multerConfig.fileFilter}).single('image'));
 app.use(cors(corsConfig));
 app.use(helmet());
+app.use(morgan('combined', {stream: morganLogStream}));
 
 //Initialize route
 app.use(require('./routes/index'));
 
+// https.createServer({key: privateKey, cert: publicKey}).listen(port, () => {
+//     console.log('Server is running');
+// })
 server.listen(port, () => {
     console.log('Server is running');
 })
